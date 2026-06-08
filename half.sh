9bf1bd212fd5c22997bca4cff4a6b0d273989d17
Anich <anichhaveatry@gmail.com>
2026-06-08 16:06:04 +0800

    init

---
+++ half.sh
@@ -0,0 +1,20 @@
+for f in *.{mp4,mkv,avi,mov,flv,webm}; do
+[ -e "$f" ] || continue
+ffmpeg -i "$f" -vf "scale=trunc(iw/2/2)*2:trunc(ih/2/2)*2" -c:v libx264 -crf 26 -preset slow -c:a copy "half_$f"
+done
+
+# 画质控制
+# -crf 23
+# CRF 数值：
+# 18 = 高质量大文件
+# 23 = 默认平衡
+# 28 = 小文件低画质
+
+# 编码速度
+# -preset medium
+# 速度/压缩率平衡：
+# ultrafast（最快）
+# fast
+# medium（默认）
+# slow
+# veryslow（最省空间）
---
+++ name.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in half_*; do
+    [ -e "$file" ] || continue
+
+    new_name="${file#half_}"
+
+    mv -- "$file" "$new_name"
+done
+
+# 将当前目录中文件名为half_*的文件全部改为*
