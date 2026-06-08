26ae1e293d306a209cf462315a6b25c1070f7084
Anich <anichhaveatry@gmail.com>
2026-06-08 19:31:26 +0800

    提高压缩率

--- noscale.sh
+++ noscale.sh
@@ -1,4 +1,4 @@
 for f in *.{mp4,mkv,avi,mov,flv,webm}; do
 [ -e "$f" ] || continue
-ffmpeg -hwaccel cuda -i "$f" -c:v libx264 -crf 26 -preset slow -c:a copy "half_$f"
+ffmpeg -hwaccel cuda -i "$f" -c:v libx264 -crf 28 -preset veryslow -c:a copy "half_$f"
 done
