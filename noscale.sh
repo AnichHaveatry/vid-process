0c917f23d90e6b4f8301cbde4b6f821913a7eae5
Anich <anichhaveatry@gmail.com>
2026-06-08 16:08:24 +0800

    use cuda

--- noscale.sh
+++ noscale.sh
@@ -1,4 +1,4 @@
 for f in *.{mp4,mkv,avi,mov,flv,webm}; do
 [ -e "$f" ] || continue
-ffmpeg -i "$f" -c:v libx264 -crf 26 -preset slow -c:a copy "half_$f"
+ffmpeg -hwaccel cuda -i "$f" -c:v libx264 -crf 26 -preset slow -c:a copy "half_$f"
 done
