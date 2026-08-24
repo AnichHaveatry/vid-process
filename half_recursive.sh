af51e5ddb66f48a81875b5cdc3d1f813218c179e
Anich <anichhaveatry@gmail.com>
2026-08-24 22:00:49 +0800

    加入递归处理版本的分辨率减半脚本

---
+++ half_recursive.sh
@@ -0,0 +1,14 @@
+find . -type f \( -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" -o -iname "*.mov" -o -iname "*.flv" -o -iname "*.webm" \) -print0 | while IFS= read -r -d '' f; do
+    dir=$(dirname "$f")
+    filename=$(basename "$f")
+    ffmpeg -hide_banner \
+        -nostdin -y \
+        -threads 0 \
+        -i "$f" \
+        -vf "scale=trunc(iw/2/2)*2:trunc(ih/2/2)*2" \
+        -c:v h264_nvenc \
+        -pix_fmt yuv420p \
+        -preset p7 \
+        -cq 35 \
+        -c:a copy "$dir/half_$filename"
+done
